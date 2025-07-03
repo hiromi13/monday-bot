3561f2f7da2ea446c753a0152355c67f18f5316e
hiromi <hiromi@hirominoMacBook-Pro.local>
2025-07-03 17:12:09 +0900

    add /callback endpoint

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Monday says hi"
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    return "OK"  # まずはテスト用に200 OKを返すだけにする
 
 if __name__ == "__main__":
     app.run()
